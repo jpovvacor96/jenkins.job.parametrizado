8baeca427c2d36c71142eefb69d68e91afab4d6e
USERSAD\jpalomio <jpalomio@emeal.nttdata.com>
2024-02-16 14:37:29 +0100

    Primer push de prueba para probar la ejecución del Job de Jenkins

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,5 +1,5 @@
 job('ejemplo3-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
+	description('Job DSL de ejemplo para el curso de Jenkins. Vamos a probar ahora el push')
   	scm {
       		git('https://github.com/jpovvacor96/jenkins.job.parametrizado.git', 'main') { node ->
         		node / gitConfigName('jpovvacor96')
